e6d4f800393a0f3e1106113ef4fa07a469c4ef30
Simon Wanner <skyrising@pvpctutorials.de>
2022-02-11 20:35:28 +0100

    Generate graph based on git index

--- utils.mjs
+++ utils.mjs
@@ -1,4 +1,9 @@
+import fs from 'fs'
+import path from 'path'
 import cp from 'child_process'
+
+const versionDataDir = path.resolve('mc-versions', 'data')
+const versionDir = path.resolve(versionDataDir, 'version')
 
 export function spawn(program, args, opts) {
     return new Promise((resolve, reject) => {
@@ -8,4 +13,42 @@ export function spawn(program, args, opts) {
             else resolve(code)
         })
     })
+}
+
+export function spawnText(program, args, opts) {
+    return new Promise((resolve, reject) => {
+        const buffers = []
+        const c = cp.spawn(program, args, opts)
+        c.stdout.on('data', buf => buffers.push(buf))
+        c.on('exit', code => {
+            if (code) reject(code)
+            else resolve(Buffer.concat(buffers).toString('utf8'))
+        })
+    })
+}
+
+const ERAS = {
+    inf: 'infdev',
+    in: 'indev',
+    af: 'april-fools',
+    a: 'alpha',
+    b: 'beta',
+    c: 'classic',
+    rd: 'pre-classic'
+}
+
+export function getEra(version) {
+    for (const key in ERAS) {
+        if (version.startsWith(key)) return ERAS[key]
+    }
+    const releaseTarget = getVersionDetails(version).releaseTarget
+    if (releaseTarget && /^\d+\.\d+/.test(releaseTarget)) {
+        const [, era] = releaseTarget.match(/^(\d+\.\d+)/)
+        return era
+    }
+    return releaseTarget
+}
+
+export function getVersionDetails(id) {
+    return JSON.parse(fs.readFileSync(path.resolve(versionDataDir, 'version', id + '.json')))
 }
